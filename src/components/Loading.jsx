2b1271404a96b917b13c2c3a0091e1e7cb53dd16
Trey Schneider <trey@codeschneider.com>
2018-09-11 01:04:16 -0400

    add isomorphic-fetch to package.json

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Lobby extends Component {
+export default class Loading extends Component {
 
   constructor(props) {
     super(props);
