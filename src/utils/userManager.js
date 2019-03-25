87707985b7d20eba64b5f454ea446e6be9cce0ba
Trey Schneider <trey@codeschneider.com>
2019-03-25 00:08:41 -0400

    update userManager scopes and idp endpoint

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -1,11 +1,11 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
-  authority: "http://localhost:5000",
+  authority: "http://idp.clarity.localhost",
   client_id: "ingest",
   redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback.html`,
   response_type: "code",
-  scope:"openid profile api1",
+  scope:"openid profile ingest_api solr_api",
   silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
   automaticSilentRenew: true,
   filterProtocolClaims: true,
