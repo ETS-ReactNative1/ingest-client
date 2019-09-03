b0cc215a2adb5d66ad45abb5d23f252430940df5
Trey Schneider <trey@codeschneider.com>
2019-09-03 11:54:02 -0400

    change authority, redirect_uri, silent_redirect_uri, post_logout_redirect_uri attrs

--- src/utils/userManager.js
+++ src/utils/userManager.js
@@ -1,18 +1,18 @@
 import { createUserManager } from 'redux-oidc';
 
 const userManagerConfig = {
-  authority: `${window.location.protocol}//${window._env_.REACT_APP_IDENTITY_PROVIDER_URL}`,
+  authority: `${window.location.protocol}//${window._env_.IDENTITY_PROVIDER_URL}`,
   client_id: "ingest",
-  redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback.html`,
+  redirect_uri: `${window.location.protocol}//${window._env_.INGEST_URL}/callback.html`,
   response_type: "code",
   scope:"openid profile ingest_api solr_api",
-  silent_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
+  silent_redirect_uri: `${window.location.protocol}//${window._env_.INGEST_URL}/silent_renew.html`,
   automaticSilentRenew: true,
   filterProtocolClaims: true,
   loadUserInfo: true,
   revokeAccessTokenOnSignout : true,
   query_status_response_type: "code",
-  post_logout_redirect_uri: `${window.location.protocol}//${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/csv`
+  post_logout_redirect_uri: `${window.location.protocol}//${window._env_.INGEST_URL}/csv`
 }
 
 const userManager = createUserManager(userManagerConfig);
